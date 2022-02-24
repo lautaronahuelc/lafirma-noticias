4dcd43f4773b24532451dd0024103e04c0936a2e
Lautaro Coria <lautaronahuel.coria@gmail.com>
2022-02-23 22:26:43 -0300

    Sólo se mueve la solapa

--- js/app.js
+++ js/app.js
@@ -1,11 +1,9 @@
 const buttonMenu = document.querySelector('#button-menu');
 const menuOculto = document.querySelector('#menu-oculto');
 const enlacesMenu = document.querySelectorAll('#menu-oculto a');
-const contAll = document.querySelector('#cont-all');
 
 buttonMenu.addEventListener('click', (event) => {
     menuOculto.classList.toggle('nav-visibled');
-    contAll.classList.toggle('cont-all--move');
     if(buttonMenu.getAttribute('src') === 'images/icon-menu.png') {
         buttonMenu.setAttribute('src','images/close-button.png');
     } else {
@@ -16,5 +14,4 @@ buttonMenu.addEventListener('click', (event) => {
 enlacesMenu.forEach(element => element.addEventListener('click', (event) => {
     buttonMenu.setAttribute('src','images/icon-menu.png');
     menuOculto.classList.remove('nav-visibled');
-    contAll.classList.remove('cont-all--move');
 }));
