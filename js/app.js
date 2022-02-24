ab8a2526cba4e4bc214dd5101d1ea9d6d3a264d5
Lautaro Coria <lautaronahuel.coria@gmail.com>
2022-02-23 21:53:45 -0300

    El menú se osconde una vez que se hace click en un enlace

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 const buttonMenu = document.querySelector('#button-menu');
 const menuOculto = document.querySelector('#menu-oculto');
+const enlacesMenu = document.querySelectorAll('#menu-oculto a');
 
 buttonMenu.addEventListener('click', (event) => {
     menuOculto.classList.toggle('nav-visibled');
-    if(buttonMenu.getAttribute('src') === 'images/icon-menu.png'){
+    if(buttonMenu.getAttribute('src') === 'images/icon-menu.png') {
         buttonMenu.setAttribute('src','images/close-button.png');
     } else {
         buttonMenu.setAttribute('src','images/icon-menu.png');
     }
 });
+
+enlacesMenu.forEach(element => element.addEventListener('click', (event) => {
+    buttonMenu.setAttribute('src','images/icon-menu.png');
+    menuOculto.classList.remove('nav-visibled');
+}));
