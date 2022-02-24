19821a238f3abe9110ac5a9590a9213f615c55d6
Lautaro Coria <lautaronahuel.coria@gmail.com>
2022-02-23 22:23:00 -0300

    Se agregó animación al nav para que tenga una aparición suave

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 const buttonMenu = document.querySelector('#button-menu');
 const menuOculto = document.querySelector('#menu-oculto');
 const enlacesMenu = document.querySelectorAll('#menu-oculto a');
+const contAll = document.querySelector('#cont-all');
 
 buttonMenu.addEventListener('click', (event) => {
     menuOculto.classList.toggle('nav-visibled');
+    contAll.classList.toggle('cont-all--move');
     if(buttonMenu.getAttribute('src') === 'images/icon-menu.png') {
         buttonMenu.setAttribute('src','images/close-button.png');
     } else {
@@ -14,4 +16,5 @@ buttonMenu.addEventListener('click', (event) => {
 enlacesMenu.forEach(element => element.addEventListener('click', (event) => {
     buttonMenu.setAttribute('src','images/icon-menu.png');
     menuOculto.classList.remove('nav-visibled');
+    contAll.classList.remove('cont-all--move');
 }));
